a2d19990e1ab2f1dd779e5982f0449a52772280f
Lucero Vargas <u202215301@upc.edu.pe>
2025-10-07 16:34:41 -0500

    Revert "Renzo (#1)"
    This reverts commit f2c147009e746f08f237bbf8e5ac1cf7994527da.

--- src/main/java/pe/edu/upc/agricorp/repositories/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.agricorp.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.agricorp.entities.Client;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
--- src/main/java/pe/edu/upc/agricorp/repositories/PhaseRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.agricorp.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.agricorp.entities.Phase;
-
-public interface PhaseRepository extends JpaRepository<Phase, Long> {
-}
